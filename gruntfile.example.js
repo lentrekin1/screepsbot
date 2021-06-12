486784cda29a11cc7dbd88ad13f28cf96e3f4823
lance <lancee478@gmail.com>
2021-06-12 14:27:54 -0700

    initial algorithm created

---
+++ gruntfile.example.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: '',
+                token: '',
+                branch: 'default',
+                //server: 'season'
+            },
+            dist: {
+                src: ['src/*.js']
+            }
+        }
+    });
+}
